12c7fb47a3ad5f8b891c6569b8708d65a9b11976
kahadei <kahadey2@gmail.com>
2024-05-27 14:44:40 +0300

    fixes

--- forum/views.py
+++ forum/views.py
@@ -195,6 +195,7 @@ def create_profile_view(request):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @swagger_auto_schema(
